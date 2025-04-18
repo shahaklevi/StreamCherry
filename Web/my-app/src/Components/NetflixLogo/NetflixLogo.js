9bb271c2fda6664a28a135ac048d6d0aa325e707
Hodaya <hodaya.kashkash@live.biu.ac.il>
2025-04-18 21:17:59 +0300

    update logo

--- Web/my-app/src/Components/NetflixLogo/NetflixLogo.js
+++ Web/my-app/src/Components/NetflixLogo/NetflixLogo.js
@@ -5,27 +5,13 @@ import "./NetflixLogo.css";
 function NetflixLogo() {
   return (
     <div className="logo-container">
-      <svg
-      className="streamcherry-logo"
+      <img
+        className="streamcherry-logo"
+        src="/images/logos/7.png" 
+        alt="StreamCherry Logo"
         width="300"
         height="100"
-        viewBox="0 8 111 70"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-label="StreamCherry Logo"
-      >
-        <text
-          x="50%"
-          y="50%"
-          fill="#E50914"
-          fontFamily="Arial, sans-serif"
-          fontWeight="bold"
-          fontSize="32"
-          dominantBaseline="middle"
-          textAnchor="middle"
-        >
-          StreamCherry
-        </text>
-      </svg>
+      />
     </div>
   );
 }
